93642a64de14eba7c2a79a6e180560b96ed6614a
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-12-25 11:57:42 -0500

    refactor(waitlist) : removed waiting list logix and UI

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import type { RegisterUserPayload, LoginUserPayload } from "../types/user";
 
 const api = axios.create({
   // baseURL: "http://localhost:8000/api",
@@ -9,26 +8,8 @@ const api = axios.create({
   },
 });
 
-export const submitEmail = async (email: string) => {
-  const respons = await api.post("/v1/waitlist/add", { email });
-  return respons.data;
-};
-
-export const registerUser = async ({
-  name,
-  email,
-  password,
-}: RegisterUserPayload) => {
-  const response = await api.post("/v1/auth/signup", {
-    name,
-    email,
-    password,
-  });
-  return response.data;
-};
-
 export const checkEmailAvailability = async (email: string) => {
-  const response = await api.get(`/v1/profile/check-email?email=${email}`);
+  const response = await api.get(`/v1/users/check-email?email=${email}`);
 
   const isAvailable =
     response.data.message === "Email is available" ? true : false;
@@ -37,7 +18,7 @@ export const checkEmailAvailability = async (email: string) => {
 
 export const checkUsernameAvailability = async (username: string) => {
   const response = await api.get(
-    `/v1/profile/check-username?username=${username}`
+    `/v1/users/profile/check-username?username=${username}`
   );
 
   const isAvailable =
